0336bd8d899fc9f133cc5a7a3af71c6d6bd92ce9
ddaoxuan <dawid.daoxuan.uk@gmail.com>
2024-12-12 21:52:58 +0100

    2024 - d6 updated

--- 2024/d6/index.ts
+++ 2024/d6/index.ts
@@ -1,6 +1,13 @@
 import { fetchDayInput } from "../../utils/fetch-input";
 type Directions = "up" | "down" | "left" | "right";
 
+const DIRECTIONS = {
+  up: { x: 0, y: -1 },
+  down: { x: 0, y: 1 },
+  left: { x: -1, y: 0 },
+  right: { x: 1, y: 0 },
+};
+
 async function p1() {
   const input = await fetchDayInput({
     day: 6,
@@ -8,26 +15,37 @@ async function p1() {
   });
 
   const grid = parse(input);
-  let hasNextPosition = true;
-  let currentDirection: Directions = "up";
+  const guard = findGuard(grid)!;
+  const visited = new Set();
 
-  while (hasNextPosition) {
-    const guard = findGuard(grid)!;
-    const nextPosition = nextValidPos(grid, guard, currentDirection);
+  let direction: Directions = "up";
+  let gx = guard.x;
+  let gy = guard.y;
 
-    if (!nextPosition) {
-      hasNextPosition = false;
-      grid[guard.y][guard.x] = "X";
+  while (true) {
+    const key = `${gx} ${gy}`;
+
+    if (isOutOfBounds(grid, gx, gy, direction)) {
+      visited.add(key);
+
       break;
     }
 
-    grid[guard.y][guard.x] = "X";
-    grid[nextPosition.y][nextPosition.x] = "^";
-    currentDirection = nextPosition.direction as Directions;
+    if (isInvalid(grid, gx, gy, direction)) {
+      direction = changeDirection(direction);
+
+      continue;
+    }
+
+    const nx = gx + DIRECTIONS[direction].x;
+    const ny = gy + DIRECTIONS[direction].y;
+    const nextPosKey = `${nx} ${ny}`;
+    gx = nx;
+    gy = ny;
+    visited.add(nextPosKey);
   }
 
-  const total = countPath(grid);
-  console.log({ total });
+  console.log({ visited: visited.size });
 }
 
 async function p2() {
@@ -37,82 +55,97 @@ async function p2() {
   });
 
   const grid = parse(input);
-  const visitedPath = new LinkedList();
+  const guard = findGuard(grid)!;
 
   let obstacles = 0;
 
   for (let row = 0; row < grid.length; row++) {
-    for (let cell = 0; cell < grid[0].length; cell++) {
-      // add single obstacle and test if cycle exists
-      let hasNextPosition = true;
-      let currentDirection: Directions = "up";
-      grid[row][cell] = "0";
+    for (let col = 0; col < grid[0].length; col++) {
+      let direction: Directions = "up";
+      let gx = guard.x;
+      let gy = guard.y;
+      const visited = new Set();
 
-      while (hasNextPosition) {
-        const guard = findGuard(grid)!;
-        const nextPosition = nextValidPos(grid, guard, currentDirection);
-        const hasCycle = nextPosition
-          ? detectCycle(
-              new Node([nextPosition.y, nextPosition.x], visitedPath.tail),
-            )
-          : false;
+      while (true) {
+        if (isOutOfBounds(grid, gx, gy, direction)) {
+          break;
+        }
 
-        console.log({ nextPosition });
+        if (isInvalidWithReplacement(grid, gx, gy, direction, col, row)) {
+          direction = changeDirection(direction);
 
-        if (hasCycle) {
+          continue;
+        }
+
+        const nx = gx + DIRECTIONS[direction].x;
+        const ny = gy + DIRECTIONS[direction].y;
+        const nextPosKey = `${nx} ${ny} ${direction}`;
+
+        if (visited.has(nextPosKey)) {
           obstacles += 1;
-          hasNextPosition = false;
-          grid[guard.y][guard.x] = "X";
           break;
         }
 
-        if (!nextPosition) {
-          hasNextPosition = false;
-          grid[guard.y][guard.x] = "X";
-          break;
-        }
-
-        visitedPath.append([nextPosition.y, nextPosition.x]);
-        grid[guard.y][guard.x] = "X";
-        grid[nextPosition.y][nextPosition.x] = "^";
-        currentDirection = nextPosition.direction as Directions;
+        gx = nx;
+        gy = ny;
+        visited.add(nextPosKey);
       }
     }
   }
+
   console.log({ obstacles });
 }
 
-function detectCycle(head: Node) {
-  if (!head || !head.next) {
-    return false;
+function changeDirection(direction: Directions): Directions {
+  switch (direction) {
+    case "up":
+      return "right";
+    case "right":
+      return "down";
+    case "down":
+      return "left";
+    case "left":
+      return "up";
   }
-
-  let slow = head;
-  let fast = head;
-
-  while (fast && fast.next) {
-    if (slow === fast) {
-      return true;
-    }
-
-    //@ts-ignore
-    slow = slow.next;
-    //@ts-ignore
-    fast = fast.next.next;
-  }
-
-  return false;
 }
 
-function parse(input: string) {
-  return input
-    .split("\n")
-    .filter(Boolean)
-    .map((r) => r.split("").filter(Boolean));
+function isOutOfBounds(
+  grid: string[][],
+  gx: number,
+  gy: number,
+  direction: Directions,
+) {
+  const ny = gy + DIRECTIONS[direction].y;
+  const nx = gx + DIRECTIONS[direction].x;
+
+  return ny >= grid.length || nx >= grid[0].length || ny < 0 || nx < 0;
 }
 
-//p1().catch(console.error);
-p2().catch(console.error);
+function isInvalidWithReplacement(
+  grid: string[][],
+  gx: number,
+  gy: number,
+  direction: Directions,
+  cx: number,
+  cy: number,
+) {
+  const ny = gy + DIRECTIONS[direction].y;
+  const nx = gx + DIRECTIONS[direction].x;
+
+  return grid[ny][nx] === "#" || (ny === cy && nx === cx);
+}
+
+function isInvalid(
+  grid: string[][],
+  gx: number,
+  gy: number,
+  direction: Directions,
+) {
+  const ny = gy + DIRECTIONS[direction].y;
+  const nx = gx + DIRECTIONS[direction].x;
+
+  return grid[ny][nx] === "#";
+}
 
 function findGuard(grid: string[][]) {
   for (let y = 0; y < grid?.length; y++) {
@@ -122,164 +155,16 @@ function findGuard(grid: string[][]) {
       }
     }
   }
+
+  return null;
 }
 
-function nextValidPos(
-  grid: string[][],
-  guardPos: { x: number; y: number },
-  direction: Directions = "up",
-) {
-  // upwards default if not rotate to right 90deg
-  const nextUp = grid?.[guardPos.y - 1]?.[guardPos.x];
-  const nextDown = grid?.[guardPos.y + 1]?.[guardPos.x];
-  const nextLeft = grid?.[guardPos.y]?.[guardPos.x - 1];
-  const nextRight = grid?.[guardPos.y]?.[guardPos.x + 1];
-  console.log({ nextUp, nextDown, nextLeft, nextRight, direction });
-
-  if (!nextUp || !nextDown || !nextLeft || !nextRight) return;
-
-  switch (direction) {
-    case "up":
-      return [
-        { content: nextUp, y: guardPos.y - 1, x: guardPos.x, direction: "up" },
-        {
-          content: nextRight,
-          y: guardPos.y,
-          x: guardPos.x + 1,
-          direction: "right",
-        },
-        {
-          content: nextDown,
-          y: guardPos.y + 1,
-          x: guardPos.x,
-          direction: "down",
-        },
-        {
-          content: nextLeft,
-          y: guardPos.y,
-          x: guardPos.x - 1,
-          direction: "left",
-        },
-      ].find(({ content }) => content === "." || content === "X");
-
-    case "right":
-      return [
-        {
-          content: nextRight,
-          y: guardPos.y,
-          x: guardPos.x + 1,
-          direction: "right",
-        },
-        {
-          content: nextDown,
-          y: guardPos.y + 1,
-          x: guardPos.x,
-          direction: "down",
-        },
-        {
-          content: nextLeft,
-          y: guardPos.y,
-          x: guardPos.x - 1,
-          direction: "left",
-        },
-        { content: nextUp, y: guardPos.y - 1, x: guardPos.x, direction: "up" },
-      ].find(({ content }) => content === "." || content === "X");
-
-    case "down":
-      return [
-        {
-          content: nextDown,
-          y: guardPos.y + 1,
-          x: guardPos.x,
-          direction: "down",
-        },
-        {
-          content: nextLeft,
-          y: guardPos.y,
-          x: guardPos.x - 1,
-          direction: "left",
-        },
-        { content: nextUp, y: guardPos.y - 1, x: guardPos.x, direction: "up" },
-        {
-          content: nextRight,
-          y: guardPos.y,
-          x: guardPos.x + 1,
-          direction: "right",
-        },
-      ].find(
-        ({ content }) => content === "." || content === "X" || content === "^",
-      );
-
-    case "left":
-      return [
-        {
-          content: nextLeft,
-          y: guardPos.y,
-          x: guardPos.x - 1,
-          direction: "left",
-        },
-        { content: nextUp, y: guardPos.y - 1, x: guardPos.x, direction: "up" },
-        {
-          content: nextRight,
-          y: guardPos.y,
-          x: guardPos.x + 1,
-          direction: "right",
-        },
-        {
-          content: nextDown,
-          y: guardPos.y + 1,
-          x: guardPos.x,
-          direction: "down",
-        },
-      ].find(({ content }) => content === "." || content === "X");
-
-    default:
-      return;
-  }
+function parse(input: string) {
+  return input
+    .split("\n")
+    .filter(Boolean)
+    .map((r) => r.split("").filter(Boolean));
 }
 
-function countPath(grid: string[][]) {
-  let total = 0;
-
-  for (let y = 0; y < grid?.length; y++) {
-    for (let x = 0; x < grid[x]?.length; x++) {
-      if (grid[y][x] === "X") {
-        total += 1;
-      }
-    }
-  }
-  return total;
-}
-
-class Node {
-  constructor(
-    public data: [number, number],
-    public next: Node | null,
-  ) {
-    this.data = data;
-    this.next = next;
-  }
-}
-
-class LinkedList {
-  public head: Node | null;
-  public tail: Node | null;
-
-  constructor() {
-    this.head = null;
-    this.tail = null;
-  }
-
-  append(value: [number, number]) {
-    const newNode = new Node(value, null);
-
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = newNode;
-      return null;
-    }
-    // @ts-expect-error
-    this.tail.next = newNode;
-    this.tail = newNode;
-  }
-}
+p1().catch(console.error);
+p2().catch(console.error);
